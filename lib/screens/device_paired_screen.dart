d809b0038618d8714fcf5c87181e78c630f6fb27
Shashwat Sharma <shashwatsharma@Shashwats-MacBook-Air.local>
2021-09-29 20:35:48 +0530

    ble wifi setter

--- lib/screens/device_paired_screen.dart
+++ lib/screens/device_paired_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 
@@ -9,21 +11,94 @@ class DevicePairedScreen extends StatefulWidget {
 }
 
 class _DevicePairedScreenState extends State<DevicePairedScreen> {
+  final String SERVICE_UUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
+  final String CHARACTERISTIC_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
+  final String TARGET_DEVICE_NAME = "ESP32 THAT PROJECT";
+
+  FlutterBlue flutterBlue = FlutterBlue.instance;
+
+  late BluetoothCharacteristic targetCharacteristic;
+  String connectionText = "";
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  discoverServices() async {
+    if (widget.device == null) {
+      return;
+    }
+
+    List<BluetoothService> services = await widget.device.discoverServices();
+    services.forEach((service) {
+      if (service.uuid.toString() == SERVICE_UUID) {
+        service.characteristics.forEach((characteristics) {
+          if (characteristics.uuid.toString() == CHARACTERISTIC_UUID) {
+            targetCharacteristic = characteristics;
+            setState(() {
+              connectionText = "All Ready with ${widget.device.name}";
+            });
+          }
+        });
+      }
+    });
+  }
+
+  writeData(String data) async {
+    if (targetCharacteristic == null) return;
+
+    List<int> bytes = utf8.encode(data);
+    await targetCharacteristic.write(bytes);
+  }
+
+  submitAction() {
+    var wifiData = '${wifiNameController.text},${wifiPasswordController.text}';
+    writeData(wifiData);
+  }
+
+  TextEditingController wifiNameController = TextEditingController();
+  TextEditingController wifiPasswordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Column(
-          children: [
-            Center(
-              child: Text("DeviceName jaspal" + widget.device.name.toString()),
-            ),
-            Center(
-              child: Text("DeviceId" + widget.device.id.id.toString()),
-            ),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(connectionText),
       ),
+      body: Container(
+          child: targetCharacteristic == null
+              ? Center(
+                  child: Text(
+                    "Waiting...",
+                    style: TextStyle(fontSize: 34, color: Colors.red),
+                  ),
+                )
+              : Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: TextField(
+                        controller: wifiNameController,
+                        decoration: InputDecoration(labelText: 'Wifi Name'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: TextField(
+                        controller: wifiPasswordController,
+                        decoration: InputDecoration(labelText: 'Wifi Password'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: ElevatedButton(
+                        onPressed: submitAction,
+                      child: Text('Submit'),
+                      ),
+                    )
+                  ],
+                )),
     );
   }
 }
